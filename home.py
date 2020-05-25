7500013e9df61928b69cf667379ccf1d1d95e049
苏大川 <sudachuan@sudachuandeMacBook-Pro.local>
2020-05-25 14:52:50 +0800

    change upload placeholder

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 
 @app.route('/')
 def home():
